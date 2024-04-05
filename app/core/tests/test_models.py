5b8289faea1a0e425ca79800a292e04d072a9c0b
Arya Vats <aryavats123@gmail.com>
2024-04-05 21:29:31 +0530

    fix test directory

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ Tests for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
- class ModelTests(TestCase):
+class ModelTests(TestCase):
  	"""Test Models"""
 
  	def test_create_user_with_email_successful(self):
@@ -18,4 +18,19 @@ from django.contrib.auth import get_user_model
 
  		self.assertEqual(user.email, email)
  		self.assertTrue(user.check_password(password)) #assertequal is not used because we need to compare witht the hashed password
- 		
+
+
+ 	def test_new_user_email_normalized(self):
+ 		"""Test email is normalized for new users."""
+ 		sample_emails = [
+ 			['test1@EXAMPLE.com', 'test1@example.com'],
+ 			['Test2@Example.com',  'Test2@example.com'],
+ 			['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+ 			['test4@example.COM', 'test4@example.com'],
+ 		]
+ 		for email, expected in sample_emails:
+ 			user = get_user_model().objects.create_user(email, 'sample123')
+ 			self.assertEqual(user.email, expected)
+
+
+ 			
