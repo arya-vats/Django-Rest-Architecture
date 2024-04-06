0abc7b30888ac8f01b69580c2b7d9e289f3a760f
Arya Vats <aryavats123@gmail.com>
2024-04-06 19:42:34 +0530

    add superuser + test blank emails

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,4 +33,17 @@ class ModelTests(TestCase):
  			self.assertEqual(user.email, expected)
 
 
- 			
+ 	def test_new_user_without_email_raises_error(self):
+ 		"""Testing that creating user without email raises a ValueError"""
+ 		with self.assertRaises(ValueError):
+ 			get_user_model().objects.create_user('', 'test123')
+
+ 	def test_create_superuser(self):
+ 		"""Test creating a superuser"""
+ 		user = get_user_model().objects.create_superuser(
+ 			'test@example.com',
+ 			'test123',
+ 		)
+
+ 		self.assertTrue(user.is_superuser)
+ 		self.assertTrue(user.is_staff)
