54dda9dcbcb6b1e172c792f9042c25616001c86c
Arya Vats <aryavats123@gmail.com>
2024-04-14 21:21:38 +0530

    add test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
+"""
+Django admin customisation
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+	"""Define the admin page for users."""
+	ordering = ['id']
+	list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)	
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,3 +23,12 @@ class AdminSiteTests(TestCase):
 			password="testpass123",
 			name="Test User",
 		)
+
+	def test_users_list(self):
+		"""Test that users are listed on page"""
+
+		url = reverse('admin:core_user_changelist')
+		res = self.client.get(url)
+
+		self.assertContains(res, self.user.name)
+		self.assertContains(res, self.user.email)
