eadc4e91fcbda29ce3ee0489be31af57e6863d2e
Arya Vats <aryavats123@gmail.com>
2024-04-18 15:01:20 +0530

    change admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,7 @@ Django admin customisation
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 
@@ -11,6 +11,21 @@ class UserAdmin(BaseUserAdmin):
 	"""Define the admin page for users."""
 	ordering = ['id']
 	list_display = ['email', 'name']
+	fieldsets = (
+		(None, {'fields': ('email', 'password')}),
+		(
+			_('Permissions'),
+			{
+				'fields': (
+					'is_active',
+					'is_staff',
+					'is_superuser',
+				)
+			}
+		),
+		(_('Important dates'), {'fields': ('last_login',)}),
+	)
+	readonly_fields= ['last_login']
 
 
-admin.site.register(models.User, UserAdmin)	
+admin.site.register(models.User, UserAdmin)
