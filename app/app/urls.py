e15e2a87257ecc33cfe7f25b6996648462f77df5
Arya Vats <aryavats123@gmail.com>
2024-04-26 22:48:22 +0530

    add tests and create api

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -27,5 +27,6 @@ urlpatterns = [
     path('api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
 ]
--- app/user/urls.py
+++ app/user/urls.py
@@ -2,5 +2,12 @@
 URL mappings for the user API.
 """
 
-from django.urls import re_path
+from django.urls import path
+
 from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+	path('create/', views.CreateUserView.as_view(), name='create'),
+]
