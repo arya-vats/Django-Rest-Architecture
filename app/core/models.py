439138622a9d11658e11e3492d0936cc50dfeb01
Arya Vats <aryavats123@gmail.com>
2024-05-03 19:54:58 +0530

    add recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
 	AbstractBaseUser,
@@ -46,3 +47,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 ##this self.model in the database is essentially maaping our own user model to that of its build in model
 ##self._db is merely used to provide support for multiple databases.
+
+class Recipe(models.Model):
+	"""Recipe object."""
+	user = models.ForeignKey(
+		settings.AUTH_USER_MODEL, #best practice is to import from settings.py file to make sure that change in model changes it everywhere in our models
+		on_delete=models.CASCADE,
+	)
+	title = models.CharField(max_length=255),
+	description = models.TextField(blank=True),
+	price = models.DecimalField(max_digits=5, decimal_places=2),
+	link = models.CharField(max_length=255, blank=True)
+
+
+	def __str__(self):
+		return self.title
