6cb2ca0ce4a4f2918fc02a029145685b19c1f954
Arya Vats <aryavats123@gmail.com>
2024-05-04 18:39:09 +0530

    Add views for recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,7 +13,7 @@ from recipe import serializers
 class RecipeViewSet(viewsets.ModelViewSet):
     """Views for managing the recipe APIS"""
 
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -21,3 +21,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for the authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return the serializer class for the request."""
+        if self.action == "list":
+            return serializers.RecipeSerializer
+        return self.serializer_class
